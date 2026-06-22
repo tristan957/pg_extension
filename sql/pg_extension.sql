6a33339293fa31229c4c7980a3d5fba6e39cc83b
Tristan Partin <tristan@partin.io>
2026-06-22 21:27:35 +0000

    Remove year from the copyright notice
    Signed-off-by: Tristan Partin <tristan@partin.io>

--- sql/pg_extension.sql
+++ sql/pg_extension.sql
@@ -1,5 +1,5 @@
 -- SPDX-License-Identifier: CC0-1.0
 --
--- SPDX-FileCopyrightText: 2024 Tristan Partin <tristan@partin.io>
+-- SPDX-FileCopyrightText: Tristan Partin <tristan@partin.io>
 
 \echo Use "CREATE EXTENSION pg_extension" to load this file. \quit
